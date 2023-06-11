4edd9d650a7d698af7274e18b602d14db38d31d1
Patrick Ducusin <pducusin123@gmail.com>
2023-06-11 18:16:42 -0400

    Install deps and configure tailwind css

--- next.config.mjs
+++ next.config.mjs
@@ -14,9 +14,12 @@ const config = {
    *
    * @see https://github.com/vercel/next.js/issues/41980
    */
-  i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
+  experimental: {
+    appDir: true,
   },
+  // i18n: {
+  //   locales: ["en"],
+  //   defaultLocale: "en",
+  // },
 };
 export default config;
