d40e1f2313ae4e913da96926aafa3698a733497c
Patrick Ducusin <pducusin123@gmail.com>
2023-06-11 18:55:55 -0400

    Create an icons component

---
+++ src/components/icons.tsx
@@ -0,0 +1,14 @@
+import {
+  Command,
+  Github,
+  Linkedin,
+  type Icon as LucideIcon,
+} from "lucide-react";
+
+export type Icon = LucideIcon;
+
+export const Icons = {
+  logo: Command,
+  github: Github,
+  linkedin: Linkedin,
+};
