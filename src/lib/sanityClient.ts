6418af8571cbe1ec50a7c34a7b1d1d4822bfef1c
Patrick Ducusin <pducusin123@gmail.com>
2023-06-11 18:52:37 -0400

    Add @sanity/image-url and configure (comment only)

---
+++ src/lib/sanityClient.ts
@@ -0,0 +1,15 @@
+// import {createClient, type ClientConfig} from '@sanity/client'
+// import imageUrlBuilder from '@sanity/image-url';
+
+// const config: ClientConfig = {
+//   projectId: 'your-project-id',
+//   dataset: 'your-dataset-name',
+//   useCdn: true, // set to `false` to bypass the edge cache
+//   apiVersion: '2023-05-03', // use current date (YYYY-MM-DD) to target the latest API version
+// }
+
+// export const client = createClient(config)
+
+// Set up image URL builder for Sanity
+// const builder = imageUrlBuilder(client);
+// export const urlFor = (source: string) => urlBuilder.image(source);
