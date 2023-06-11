26b5e0878d75ab2a14753e41e1bbe780349d1ee7
Patrick Ducusin <pducusin123@gmail.com>
2023-06-11 18:51:08 -0400

    Add basic home page

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="mx-auto px-6">
+      <p className="my-12 text-center text-3xl text-foreground">
+        Welcome to your new Next.js site.
+      </p>
+    </main>
+  );
+}
