d65aff3daec337b0dfdfe26e485b541d324692b1
Patrick Ducusin <pducusin123@gmail.com>
2023-06-12 21:55:53 -0400

    Add className to container-wrapper

--- src/components/wrapper/container-wrapper.tsx
+++ src/components/wrapper/container-wrapper.tsx
@@ -4,17 +4,21 @@
  * icons for Navigation and social media.
  */
 
+import { cn } from "@/lib/utils";
+
 interface ContainerWrapperProps {
   id: string;
   children: React.ReactNode;
+  className?: string;
 }
 
 export default function ContainerWrapper({
   children,
   id,
+  className,
 }: ContainerWrapperProps) {
   return (
-    <div id={id} className="flex min-h-screen w-full flex-row">
+    <div id={id} className={cn("flex min-h-screen w-full flex-row", className)}>
       {/* <SocialMedia /> */}
       <div className="xs:pt-16 xs:px-4 xs:pb-8 flex w-full flex-1 flex-col items-center justify-center px-8 py-16">
         {children}
