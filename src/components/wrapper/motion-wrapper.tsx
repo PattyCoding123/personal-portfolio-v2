272417f2b8dae5dd8931dfc0842bf65bb2096d77
Patrick Ducusin <pducusin123@gmail.com>
2023-06-12 21:18:38 -0400

    Create motion-wrapper component

---
+++ src/components/wrapper/motion-wrapper.tsx
@@ -0,0 +1,28 @@
+import { cn } from "@/lib/utils";
+import { motion } from "framer-motion";
+
+/**
+ * Component which will wrap the child component in a motion.div
+ * that will create an "appearing" animation whenerver the component
+ * comes into view.
+ */
+
+interface MotionWrapperProps {
+  children: React.ReactNode;
+  className?: string;
+}
+
+export default function MotionWrapper({
+  children,
+  className,
+}: MotionWrapperProps) {
+  return (
+    <motion.div
+      whileInView={{ y: [100, 50, 0], opacity: [0, 0, 1] }}
+      transition={{ duration: 0.5 }}
+      className={cn("flex items-center justify-center", className)}
+    >
+      {children}
+    </motion.div>
+  );
+}
