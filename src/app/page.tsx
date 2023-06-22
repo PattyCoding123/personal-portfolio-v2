3739c194df36bb165618b5469eff03e991c31113
Patrick Ducusin <patrickducusin2@gmail.com>
2023-06-21 21:27:15 -0400

    Remove filler text in home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,6 @@ import About from "@/components/container/about";
 export default function Home() {
   return (
     <main className="mx-auto px-6">
-      <p className="my-12 text-center text-3xl text-foreground">
-        Welcome to your new Next.js site.
-      </p>
       {/* @ts-expect-error Async Server Component */}
       <About />
     </main>
