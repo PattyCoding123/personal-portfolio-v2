18ffbdb45ad4008a65a6bf5bd826f53f30002a12
Patrick Ducusin <patrickducusin2@gmail.com>
2023-07-02 17:33:12 -0400

    Add all stylings for skills

--- src/components/container/skills/exp-list.tsx
+++ src/components/container/skills/exp-list.tsx
@@ -12,27 +12,39 @@ interface ExperiencesListProps {
 
 export default function ExperiencesList({ experiences }: ExperiencesListProps) {
   return (
-    <div className="app__skils-exp">
+    <div className="mt-2 flex flex-1 flex-col items-start justify-start lg:mt-0">
       {experiences?.map((experience) => (
-        <motion.div key={experience.year} className="app__skills-exp-item">
-          <div className="app__skills-exp-year">
-            <p className="bold-text">{experience.year}</p>
+        <motion.div
+          key={experience.year}
+          className="mx-0 my-4 flex w-full items-start justify-start"
+        >
+          <div className="mr-12 xs:mr-1">
+            <p className="text-left text-[1rem] font-extrabold text-foreground 3xl:text-[2rem] xs:text-[0.9rem]">
+              {experience.year}
+            </p>
           </div>
-          <motion.div className="app__skills-exp-works">
+          <motion.div className="flex-1">
             {experience.works.map((work, index) => (
               <Fragment key={index}>
                 <motion.div
                   key={work.name}
                   whileInView={{ opacity: [0, 1] }}
                   transition={{ duration: 0.5 }}
-                  className="app__skills-exp-work"
+                  className="mb-4 flex cursor-pointer flex-col items-start justify-start"
                   data-tooltip-id={work.name}
                   data-tooltip-content={work.desc}
                 >
-                  <h4 className="bold-text">{work.name}</h4>
-                  <p className="p-text">{work.company}</p>
+                  <h4 className="text-left text-[1rem] font-medium text-foreground 3xl:text-[2rem] xs:text-[0.9rem]">
+                    {work.name}
+                  </h4>
+                  <p className="mt-[5px] text-left text-[0.8rem] font-normal leading-6 text-foreground/80 3xl:text-[1.75rem]">
+                    {work.company}
+                  </p>
                 </motion.div>
-                <Tooltip id={work.name} />
+                <Tooltip
+                  id={work.name}
+                  className="!3xl:max-w-[500px] !3xl:text-sm !max-w-xs !rounded-sm !bg-slate-200 !p-4 !text-center !text-xs !text-slate-800 !opacity-100 !shadow-lg"
+                />
               </Fragment>
             ))}
           </motion.div>
--- src/components/container/skills/skills-list.tsx
+++ src/components/container/skills/skills-list.tsx
@@ -11,18 +11,29 @@ interface SkillsListProps {
 }
 export default function SkillsList({ skills }: SkillsListProps) {
   return (
-    <motion.div className="app__skills-list">
+    <motion.div className="mr-0 flex flex-1 flex-wrap items-center justify-center lg:mr-20 lg:items-start lg:justify-start">
       {skills?.map((skill) => (
         <motion.div
           key={skill.name}
           whileInView={{ opacity: [0, 1] }}
           transition={{ duration: 0.5 }}
-          className="app__skills-item app__flex"
+          className="m-4 flex flex-col items-center justify-center text-center transition-all duration-300 ease-in-out 3xl:mx-8 3xl:my-4"
         >
-          <div className={cn("app__flex", `${skill.bgColor}`)}>
-            <img src={urlFor(skill.icon).url()} alt={skill.name} />
-            <p className="p-text">{skill.name}</p>
+          <div
+            className={cn(
+              "flex h-24 w-24 items-center justify-center rounded-full hover:shadow-lg 3xl:h-36 3xl:w-36 xs:h-16 xs:w-16",
+              `bg-[${skill.bgColor}]`
+            )}
+          >
+            <img
+              src={urlFor(skill.icon).url()}
+              alt={skill.name}
+              className="h-1/2 w-1/2"
+            />
           </div>
+          <p className="mt-2 text-left text-[0.8rem] font-medium leading-6 text-foreground/80 3xl:mt-4 3xl:text-[1.75rem]">
+            {skill.name}
+          </p>
         </motion.div>
       ))}
     </motion.div>
