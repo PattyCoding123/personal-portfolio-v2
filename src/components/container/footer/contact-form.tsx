1b70886fe2ad4919aa7a1c6566e17f90fd80ab8c
Patrick Ducusin <patrickducusin2@gmail.com>
2023-07-02 23:58:28 -0400

    Finish form

--- src/components/container/footer/contact-form.tsx
+++ src/components/container/footer/contact-form.tsx
@@ -1,7 +1,112 @@
 "use client";
 
 import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 
-export default function ContactForm() {
-  return <></>;
+import { formSchema, type FormData } from "@/lib/validators/form-validator";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { client } from "@/lib/sanityClient";
+import { ButtonLoading } from "@/components/button-loading";
+
+export function ProfileForm() {
+  const form = useForm<FormData>({
+    resolver: zodResolver(formSchema),
+  });
+
+  const [isFormSubmitted, setIsFormSubmitted] = useState<boolean>(false);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  const onSubmit = ({ name, email, message }: FormData) => {
+    setIsLoading(true);
+
+    const contact = {
+      _type: "contact",
+      name: name,
+      email: email,
+      message: message,
+    };
+
+    void client.create(contact).then(() => {
+      setIsLoading(false);
+      setIsFormSubmitted(true);
+    });
+  };
+
+  return (
+    <>
+      {isFormSubmitted ? (
+        <div>
+          <h3 className="head-text">Thank you for getting in touch!</h3>
+        </div>
+      ) : (
+        <Form {...form}>
+          <form
+            onSubmit={void form.handleSubmit((data) => onSubmit(data))}
+            className="space-y-8"
+          >
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Username</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Name..." {...field} />
+                  </FormControl>
+                  <FormDescription>Please enter your name...</FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Username</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Email..." {...field} />
+                  </FormControl>
+                  <FormDescription>Please enter your email...</FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="message"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Username</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Message..." {...field} />
+                  </FormControl>
+                  <FormDescription>
+                    What would you like to send?
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            {isLoading ? (
+              <ButtonLoading />
+            ) : (
+              <Button type="submit">Submit</Button>
+            )}
+          </form>
+        </Form>
+      )}
+    </>
+  );
 }
