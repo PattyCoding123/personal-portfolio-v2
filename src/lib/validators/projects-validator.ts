2304ea140c806ae36a671ab63fa03b76138acf16
Patrick Ducusin <patrickducusin2@gmail.com>
2023-07-03 11:53:29 -0400

    Create projects validator

---
+++ src/lib/validators/projects-validator.ts
@@ -0,0 +1,36 @@
+import { z } from "zod";
+export const projectsSchema = z.array(
+  z.object({
+    _id: z.string(),
+    _type: z.literal("experiences"),
+    _updatedAt: z.string().optional(),
+    _createdAt: z.string().optional(),
+    _rev: z.string().optional(),
+    title: z.string(),
+    description: z.string(),
+    projectLink: z.string().optional(),
+    codeLink: z.string().optional(),
+    imgUrl: z.object({
+      _type: z.literal("image"),
+      asset: z.object({
+        _ref: z.string(),
+        _type: z.literal("reference"),
+      }),
+      hotspot: z
+        .object({
+          x: z.number(),
+          y: z.number(),
+          height: z.number(),
+          width: z.number(),
+        })
+        .optional(),
+    }),
+    tags: z.array(
+      z.object({
+        tag: z.string(),
+      })
+    ),
+  })
+);
+
+export type Projects = z.infer<typeof projectsSchema>;
