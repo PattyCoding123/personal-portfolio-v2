10d79b92b5ececa301ec91945de460fc9994e348
Patrick Ducusin <patrickducusin2@gmail.com>
2023-07-03 01:32:32 -0400

    Add new background

--- src/components/container/header/header.tsx
+++ src/components/container/header/header.tsx
@@ -17,7 +17,10 @@ const scaleVariants = {
 
 export default function Header() {
   return (
-    <ContainerWrapper id="home" className="relative bg-background p-0">
+    <ContainerWrapper
+      id="home"
+      className="relative bg-background-secondary p-0 dark:bg-background"
+    >
       <div className="flex h-full w-full flex-1 flex-col items-center justify-center px-8 pb-0 pt-24 xl:flex-row 3xl:pt-32">
         <motion.div
           whileInView={{ x: [-100, 0], opacity: [0, 1] }}
