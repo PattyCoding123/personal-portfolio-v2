57fb1c90d09268c569449f6a072e0e822bc1e3bd
Patrick Ducusin <patrickducusin2@gmail.com>
2023-07-03 12:00:46 -0400

    Fetch projects data and start on filtering component

---
+++ src/components/container/projects/projects-filter.tsx
@@ -0,0 +1,9 @@
+import { type Projects } from "@/lib/validators/projects-validator";
+
+interface ProjectsFilterProps {
+  projects: Projects;
+}
+
+export default function ProjectsFilter({ projects }: ProjectsFilterProps) {
+  return <></>;
+}
--- src/components/container/projects/projects.tsx
+++ src/components/container/projects/projects.tsx
@@ -1,3 +1,24 @@
-export default function Projects() {
+import { client } from "@/lib/sanityClient";
+import { projectsSchema } from "@/lib/validators/projects-validator";
+import { z } from "zod";
+
+const getProjects = async () => {
+  try {
+    const query = '*[_type == "projects"]';
+
+    const data = await client
+      .fetch(query)
+      .then((res) => projectsSchema.parse(res));
+
+    return data;
+  } catch (error) {
+    if (error instanceof z.ZodError) {
+      throw new Error("Invalid abouts data");
+    }
+  }
+};
+export default async function Projects() {
+  const projects = await getProjects();
+
   return <></>;
 }
