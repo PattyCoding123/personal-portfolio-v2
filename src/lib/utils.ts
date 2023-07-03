354b008f6575c4fcdf243d8819f59f860c2f3822
Patrick Ducusin <patrickducusin2@gmail.com>
2023-07-02 23:30:23 -0400

    Init shadcn project ui

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,6 @@
-import { clsx, type ClassValue } from "clsx";
-import { twMerge } from "tailwind-merge";
-
-export function cn(...args: ClassValue[]): string {
-  return twMerge(clsx(...args));
+import { type ClassValue, clsx } from "clsx"
+import { twMerge } from "tailwind-merge"
+ 
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
 }
