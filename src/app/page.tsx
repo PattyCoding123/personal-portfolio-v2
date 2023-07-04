9526c1c94fc6541aade8b8cfa1f5f4ea963ecb35
Patrick Ducusin <patrickducusin2@gmail.com>
2023-07-03 21:01:05 -0400

    Remove unused ts-expect-error for async server component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,11 +8,8 @@ export default function Home() {
   return (
     <main className="mx-auto">
       <Header />
-      {/* @ts-expect-error Async Server Component */}
       <About />
-      {/* @ts-expect-error Async Server Component */}
       <Projects />
-      {/* @ts-expect-error Async Server Component */}
       <Skills />
       <Footer />
     </main>
